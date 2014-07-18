f16614274dfd24b61c50b6cd522f2710c445c399
Apekshit <apekshit33@gmail.com>
2014-07-18 22:13:18 +0530

    Added Animations.

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function () {
+    jQuery('.bounceInDownAnime').addClass("hiddenAnime").viewportChecker({
+        classToAdd: 'visibleAnime animated bounceInDown',
+        offset: 100
+    });
+
+    jQuery('.bounceInUpAnime').addClass("hiddenAnime").viewportChecker({
+        classToAdd: 'visibleAnime animated bounceInUp',
+        offset: 100
+    });
+
+    jQuery('.fadeInLeftAnime').addClass("hiddenAnime").viewportChecker({
+        classToAdd: 'visibleAnime animated fadeInLeft',
+        offset: 100
+    });
+
+    jQuery('.fadeInRightAnime').addClass("hiddenAnime").viewportChecker({
+        classToAdd: 'visibleAnime animated fadeInRight',
+        offset: 100
+    });
+
+    jQuery('.zoomInUpAnime').addClass("hiddenAnime").viewportChecker({
+        classToAdd: 'visibleAnime animated zoomInUp',
+        offset: 100
+    });
+
+    jQuery('.bounceInLeftAnime').addClass("hiddenAnime").viewportChecker({
+        classToAdd: 'visibleAnime animated bounceInLeft',
+        offset: 100
+    });
+
+    jQuery('.bounceInRightAnime').addClass("hiddenAnime").viewportChecker({
+        classToAdd: 'visibleAnime animated bounceInRight',
+        offset: 100
+    });
+
+    jQuery('.pulseAnime').addClass("hiddenAnime").viewportChecker({
+        classToAdd: 'visibleAnime animated pulse',
+        offset: 100
+    });
+});
