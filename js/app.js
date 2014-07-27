a88211cacd354da74aecbe86f9317963f8c0ff69
Apekshit <apekshit33@gmail.com>
2014-07-27 20:23:13 +0530

    Viewport Correction.
    Solved issue of navbar obscuring the content when anchoring within the
    page.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-$(document).ready(function () {
-    $("div.clickable").on("click", function () {
+$(document).ready(function() {
+    $("div.clickable").on("click", function() {
         var service = this.getAttribute("data-serviceMessage");
         var next = $(this).data('value');
         $("#app").children("div").addClass("hidden");
         $("#" + next).removeClass("hidden");
-        $(document).scrollTo("#" + next);
     });
 });
