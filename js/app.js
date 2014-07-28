229bb110e5b2553cf6d042f92b2dc5f1bdc89bcb
Apekshit <apekshit33@gmail.com>
2014-07-28 20:45:23 +0530

    Added Cloud Computing Services Page.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-$(document).ready(function() {
-    $("div.clickable").on("click", function() {
-        var service = this.getAttribute("data-serviceMessage");
-        var next = $(this).data('value');
-        $("#app").children("div").addClass("hidden");
-        $("#" + next).removeClass("hidden");
+$(document).ready(function () {
+    $("div.clickable").on("click", function () {
+        var service = this.getAttribute("data-service");
+        var sub_service = this.getAttribute("data-subService");
+        $("#subServices").addClass("hidden");
+        $("#contactForm").removeClass("hidden");
+        document.getElementById("interest").innerHTML = sub_service;
+    });
+
+    $("a#nxtBtn").on("click", function () {
+        var service = this.getAttribute("data-service");
+        var sub_service = document.getElementById("otherInterests").value;
+        $("#subServices").addClass("hidden");
+        $("#contactForm").removeClass("hidden");
+        document.getElementById("interest").innerHTML = sub_service;
     });
 });
